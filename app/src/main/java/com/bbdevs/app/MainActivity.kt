b9c1f9baf6b202a8c90f4409e59342bf09eed293
Nazarii Klymok <nazarii.klymok@botscrew.com>
2021-11-20 00:08:52 +0200

    added remove button

--- app/src/main/java/com/bbdevs/app/MainActivity.kt
+++ app/src/main/java/com/bbdevs/app/MainActivity.kt
@@ -56,6 +56,10 @@ class MainActivity : AppCompatActivity() {
             val bmp = BitmapFactory.decodeStream(inStream)
             apiCatImg.setImageBitmap(bmp)
         }
+
+        removeCatPicBtn.setOnClickListener {
+            apiCatImg.setImageBitmap(null)
+        }
 //        addUserInfo.setOnClickListener{
 //            val balance = enterBalance.text.toString().toInt()
 //            val catHealth = enterCatHealth.text.toString().toInt()
