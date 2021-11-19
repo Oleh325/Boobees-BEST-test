ee44f553b1e3b625890eba2659e799d1593a2b10
DESKTOP-POSS0RQ\olega <olegackiv956@gmail.com>
2021-11-19 18:03:29 +0200

    Added "create task" popup(not working yet)

--- app/src/main/java/com/bbdevs/app/MainActivity.kt
+++ app/src/main/java/com/bbdevs/app/MainActivity.kt
@@ -4,65 +4,19 @@ import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.os.Bundle
-import android.view.View
-import android.widget.DatePicker
-import android.widget.TimePicker
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.bbdevs.app.entity.Task
 import com.bbdevs.app.entity.UserInfo
-import com.bbdevs.app.service.TaskService
 import com.bbdevs.app.service.UserInfoService
-import com.bbdevs.app.util.DateTimeUtil
 import kotlinx.android.synthetic.main.activity_main.*
-import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.LocalTime
-import java.util.*
 
-class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
-
-    private lateinit var pickedDeadline: LocalDateTime
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val db = CatAppDB(this, null)
-        val taskService = TaskService(db)
         val userInfoService = UserInfoService(db)
-        setListeners(taskService, userInfoService, addTask, printTask, addUserInfo, printUserInfo)
-    }
-
-    @SuppressLint("Range")
-    private fun setListeners(taskService: TaskService, userInfoService: UserInfoService, addTask: View, printTask: View, addUserInfo: View, printUserInfo: View) {
-        addTask.setOnClickListener {
-            val name = enterName.text.toString()
-            val description = enterDescription.text.toString()
-            val reward = enterReward.text.toString().toInt()
-            val deadline = pickedDeadline
-            if (name.isEmpty()) {
-                Toast.makeText(this, "Task name or reward cannot be empty!", Toast.LENGTH_LONG).show()
-            }
-            taskService.add(Task(null, name, description, reward, deadline, isCompleted = false))
-            Toast.makeText(this, "$name added to database", Toast.LENGTH_LONG).show()
-            enterName.text.clear()
-            enterDescription.text.clear()
-            enterReward.text.clear()
-        }
-
-        addDeadline.setOnClickListener {
-            pickDate()
-        }
-
-        printTask.setOnClickListener{
-            val tasks = taskService.get()
-            for (task in tasks) {
-                Name.append("${task.name}\n")
-                Description.append("${task.description}\n")
-                Reward.append("${task.reward}\n")
-                Deadline.append("${DateTimeUtil.toHumanReadableFormat(task.deadline)}\n")
-            }
-        }
 
         addUserInfo.setOnClickListener{
             val balance = enterBalance.text.toString().toInt()
@@ -78,26 +32,5 @@ class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener, Ti
             Balance.append("${userInfo.balance}\n")
             CatHealth.append("${userInfo.catHealth}\n")
         }
-    }
-
-    private fun pickDate() {
-        val cal = Calendar.getInstance()
-        DatePickerDialog(this, this,
-            cal.get(Calendar.YEAR),
-            cal.get(Calendar.MONTH),
-            cal.get(Calendar.DAY_OF_MONTH)).show()
-    }
-
-    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        pickedDeadline = LocalDateTime.of(LocalDate.of(year, month, dayOfMonth), LocalTime.now())
-        val cal = Calendar.getInstance()
-        TimePickerDialog(this, this,
-            cal.get(Calendar.HOUR),
-            cal.get(Calendar.MINUTE),
-            true).show()
-    }
-
-    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
-        pickedDeadline = LocalDateTime.of(pickedDeadline.toLocalDate(), LocalTime.of(hourOfDay, minute))
     }
 }
