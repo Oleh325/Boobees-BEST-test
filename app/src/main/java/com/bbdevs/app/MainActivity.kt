7be202b5a5b7725778decdd39a21fcc21dba147e
Nazarii Klymok <nazarii.klymok@botscrew.com>
2021-11-19 20:45:20 +0200

    added feeding a cat

--- app/src/main/java/com/bbdevs/app/MainActivity.kt
+++ app/src/main/java/com/bbdevs/app/MainActivity.kt
@@ -3,7 +3,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.bbdevs.app.entity.UserInfo
 import com.bbdevs.app.service.UserInfoService
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,9 +12,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val db = CatAppDB(this, null)
         val userInfoService = UserInfoService(db)
+
         addTask.setOnClickListener {
             val intent = Intent(this, CreateTaskActivity::class.java)
             startActivity(intent)
+        }
+
+        updateFoodBar(userInfoService)
+
+        feedMeBtn.setOnClickListener {
+            val userInfo = userInfoService.get()
+            if (userInfo.balance > 0) {
+                if (userInfo.catHealth < 35) {
+                    feedCat(userInfoService)
+                } else {
+                    Toast.makeText(this, "Your cat is filled :)\nCome feed it next week!", Toast.LENGTH_LONG)
+                        .show()
+                }
+            } else {
+                Toast.makeText(this, "You have no cat food :(\nComplete some tasks!", Toast.LENGTH_LONG)
+                    .show()
+            }
         }
 
 //        addUserInfo.setOnClickListener{
@@ -32,5 +49,19 @@ class MainActivity : AppCompatActivity() {
 //            Balance.append("${userInfo.balance}\n")
 //            CatHealth.append("${userInfo.catHealth}\n")
 //        }
+    }
+
+    private fun feedCat(userInfoService: UserInfoService) {
+        val info = userInfoService.get()
+        info.catHealth++
+        info.balance--
+        userInfoService.update(info)
+        updateFoodBar(userInfoService)
+    }
+
+    private fun updateFoodBar(userInfoService: UserInfoService) {
+        val info = userInfoService.get()
+        foodBar.max = 35
+        foodBar.progress = info.catHealth
     }
 }
