2a3fa930901fe6e5685b243b916de2d94f7534c1
Nazarii Klymok <nazarii.klymok@botscrew.com>
2021-11-19 19:03:11 +0200

    refactored code

--- app/src/main/java/com/bbdevs/app/CreateTaskActivity.kt
+++ app/src/main/java/com/bbdevs/app/CreateTaskActivity.kt
@@ -11,7 +11,6 @@ import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.view.animation.DecelerateInterpolator
 import android.widget.DatePicker
 import android.widget.TimePicker
@@ -20,10 +19,7 @@ import androidx.annotation.RequiresApi
 import androidx.core.graphics.ColorUtils
 import com.bbdevs.app.entity.Task
 import com.bbdevs.app.service.TaskService
-import com.bbdevs.app.service.UserInfoService
-import com.bbdevs.app.util.DateTimeUtil
 import kotlinx.android.synthetic.main.activity_create_task.*
-import kotlinx.android.synthetic.main.activity_main.*
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
@@ -39,13 +35,13 @@ class CreateTaskActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListen
         setContentView(R.layout.activity_create_task)
         val db = CatAppDB(this, null)
         val taskService = TaskService(db)
-        setListeners(taskService, addTask, printTask)
+        setListeners(taskService)
         addDeadline.setOnClickListener {
             pickDate()
         }
         val alpha = 100 //between 0-255
         val alphaColor = ColorUtils.setAlphaComponent(Color.parseColor("#000000"), alpha)
-        val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), Color.TRANSPARENT, alphaColor)
+        val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), alphaColor, Color.TRANSPARENT)
         colorAnimation.duration = 500 // milliseconds
         colorAnimation.addUpdateListener { animator ->
             create_task_background_container.setBackgroundColor(animator.animatedValue as Int)
@@ -55,10 +51,7 @@ class CreateTaskActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListen
         create_task_border.animate().alpha(1f).setDuration(500).setInterpolator(
             DecelerateInterpolator()
         ).start()
-        cancel_button.setOnClickListener(){
-            val alpha = 100 // between 0-255
-            val alphaColor = ColorUtils.setAlphaComponent(Color.parseColor("#000000"), alpha)
-            val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), alphaColor, Color.TRANSPARENT)
+        cancel_button.setOnClickListener {
             colorAnimation.duration = 500 // milliseconds
             colorAnimation.addUpdateListener { animator ->
                 create_task_background_container.setBackgroundColor(
@@ -78,10 +71,7 @@ class CreateTaskActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListen
             })
             colorAnimation.start()
         }
-        create_button.setOnClickListener(){
-            val alpha = 100 // between 0-255
-            val alphaColor = ColorUtils.setAlphaComponent(Color.parseColor("#000000"), alpha)
-            val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), alphaColor, Color.TRANSPARENT)
+        create_button.setOnClickListener {
             colorAnimation.duration = 500 // milliseconds
             colorAnimation.addUpdateListener { animator ->
                 create_task_background_container.setBackgroundColor(
@@ -105,7 +95,7 @@ class CreateTaskActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListen
     }
 
     @SuppressLint("Range")
-    private fun setListeners(taskService: TaskService, addTask: View, printTask: View) {
+    private fun setListeners(taskService: TaskService) {
         create_button.setOnClickListener {
             val name = enterName.text.toString()
             val description = enterDescription.text.toString()
